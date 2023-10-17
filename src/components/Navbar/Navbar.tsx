3f0cf1a290ef75dba9e1e4224011f186b05c5163
logonoff <logonoff@users.noreply.github.com>
2023-10-17 03:48:18 -0400

    update navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,18 @@
 import { GenerateInitalsAvatarProps } from "@/components"
 import styles from "./Navbar.module.css"
 import { Persona } from "@fluentui/react-components"
-import { UserContext } from "@/app/contexts/UserContext"
+import { UserContext } from "@/contexts/UserContext"
 import { useContext } from "react"
+import Link from "next/link"
 
 export const Navbar = () => {
     const defaultUser = useContext(UserContext);
 
     return (
         <header className={styles.navbar}>
-            <h1 className={styles.title}>Codetierlist</h1>
+            <h1 className={styles.title}>
+                <Link className={styles.brand} href="/">Codetierlist</Link>
+            </h1>
 
             <Persona
                 textPosition="before"
