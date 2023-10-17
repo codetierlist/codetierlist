5c710a725fea5679fb33ca7f96fd4753abb5af9f
logonoff <logonoff@users.noreply.github.com>
2023-10-17 03:48:01 -0400

    move contrxt

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,44 @@
+import { createContext } from "react"
+
+export declare type Course = {
+    id: string,
+    name: string,
+    code: string,
+    session: "Fall" | "Winter" | "Summer"
+}
+
+/** defines a user pre-integration */
+export declare type NotTheRealUserType = {
+    role: string,
+    name: string,
+    admin: boolean
+}
+
+export const UserContext = createContext<NotTheRealUserType>({
+    role: "admin",
+    name: "John Doe",
+    admin: true
+});
+
+export const getCourses = () => {
+    return [
+        {
+            id: "1",
+            name: "Computer Science",
+            code: "CSC108",
+            session: "Fall"
+        },
+        {
+            id: "2",
+            name: "Political Science",
+            code: "CSC109",
+            session: "Fall"
+        },
+        {
+            id: "3",
+            name: "Management Engineering",
+            code: "CSC110",
+            session: "Fall"
+        }
+    ] as Course[];
+}
