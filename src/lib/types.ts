825933a5260a9e605e298224dbd3f542666f3baf
Ido <benhaim.ido@gmail.com>
2023-10-21 23:07:27 -0400

    Fixed types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,4 @@
 export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<T>;
 
-export type Tier = "S" | "A" | "B" | "C" | "D" | "?";
+export type Tier = "S" | "A" | "B" | "C" | "D" | "F";
+export type DisplayTier = Tier | "?";
