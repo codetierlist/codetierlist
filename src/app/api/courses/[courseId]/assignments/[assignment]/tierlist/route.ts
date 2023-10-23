2b179269a2c75a55150c3d8f3c83a2c219f7e0a3
Ido <benhaim.ido@gmail.com>
2023-10-23 15:30:12 -0400

    normalized tierlist

--- src/app/api/courses/[courseId]/assignments/[assignment]/tierlist/route.ts
+++ src/app/api/courses/[courseId]/assignments/[assignment]/tierlist/route.ts
@@ -1,40 +1,58 @@
-import { getAssignment, getUser } from '@/lib/apiUtils';
+import {getAssignment, getUser} from "@/lib/apiUtils";
+import {Tier} from "@/lib/types";
 
-export type Submission = {
-    scores: {
-        id: string;
-        pass: boolean;
-        course_id: string;
-        assignment_title: string;
-        solution_author_id: string;
-        testcase_author_id: string;
-    }[];
-    author: {
-        utorid: string;
-        email: string;
-        admin: boolean;
-    };
-};
+const getMean = (data: number[]) => data.reduce((a, b) => Number(a) + Number(b)) / data.length;
 
-/**
- * Get the personalized tierlist for an assignment.
- */
-export async function GET(request: Request, { params }: {
-    params: {
-        assignment: string
-        courseId: string
-    }
+const getStandardDeviation = (data: number[]) => Math.sqrt(data.reduce((sq, n) => sq + Math.pow(n - getMean(data), 2), 0) / (data.length - 1));
+
+export async function GET (request: Request, { params }: {
+  params: {
+    assignment: string
+    courseId: string
+  }
 }) {
-    const { courseId, assignment } = params;
+    const {courseId, assignment} = params;
     const user = await getUser(request);
     const assignmentObj = await getAssignment(courseId, assignment);
-
-    return Response.json(assignmentObj.submissions.map((submission: Submission) => {
-        return {
+    const res: Record<Tier, { name: string, you: boolean }[]> = {
+        S: [],
+        A: [],
+        B: [],
+        C: [],
+        D: [],
+        F: [],
+    };
+    if (assignmentObj.submissions.length === 0) {
+        return Response.json(res);
+    }
+    const scores = assignmentObj.submissions.map(submission =>
+        ({
             you: submission.author.utorid === user.utorid,
-            name: submission.author.email[0] + submission.author.email[submission.author.email.indexOf('.') + 1],
-            // TODO possibly have to hide scores later
-            score: submission.scores.filter(x => x.pass).length / submission.scores.length
-        };
-    }));
+            name: submission.author.email[0] + submission.author.email[submission.author.email.indexOf(".") + 1],
+            score: submission.scores.filter(x => x.pass).length / submission.scores.length,
+        })
+    );
+    const mean = getMean(scores.map(x => x.score));
+    const std = getStandardDeviation(scores.map(x => x.score));
+
+    for (const score of scores) {
+        if (score.score == 0) {
+            res["F"].push(score);
+        } else if (score.score == 1 || score.score > mean + 2 * std) {
+            res["S"].push(score);
+        } else if (score.score > mean + std) {
+            res["A"].push(score);
+        } else if (score.score > mean) {
+            res["B"].push(score);
+        } else if (score.score > mean - std) {
+            res["C"].push(score);
+        } else if (score.score > mean - 2 * std) {
+            res["D"].push(score);
+        } else {
+            res["F"].push(score);
+        }
+    }
+
+
+    return Response.json(res);
 }
