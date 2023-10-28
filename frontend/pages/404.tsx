72260fb2100110ef62437b98a089c6b136d19e2c
logonoff <logonoff@users.noreply.github.com>
2023-10-28 01:09:05 -0400

    docker and yarn

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,9 +1,15 @@
 import { Display } from "@fluentui/react-text";
 
+import { Dummy } from "@/backend/src/types/global";
+
 export default function NotFound () {
+    const dummy: Dummy = {
+        dummy: '404',
+    };
+
     return (
         <main>
-            <Display>404</Display>
+            <Display>{dummy.dummy}</Display>
         </main>
     );
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import { Navbar } from '@/components'
 
 const inter = Inter({ subsets: ['latin'] })
 
