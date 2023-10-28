5cc1a83f5941b2deffb410c1d7dd361dc8bcdcd2
Ido <benhaim.ido@gmail.com>
2023-10-27 23:58:54 -0400

    Added missing types to types package

--- types/index.d.ts
+++ types/index.d.ts
@@ -26,6 +26,23 @@ export const fetchedAssignmentArgs = Prisma.validator<Prisma.AssignmentDefaultAr
     }
 });
 
+export type Assignment = Prisma.AssignmentGetPayload<{}>;
+export type Course = Prisma.CourseGetPayload<{}>;
+export type User = Prisma.UserGetPayload<{}>;
+export type Submission = Prisma.SubmissionGetPayload<{}>;
+export type Score = Prisma.ScoreGetPayload<{}>;
+export type Role = Prisma.RoleGetPayload<{}>;
+export type TestCase = Prisma.TestCaseGetPayload<{}>;
+
 export type FetchedUser = Prisma.UserGetPayload<typeof fetchedUserArgs>;
 export type FetchedCourse = Prisma.CourseGetPayload<typeof fetchedCourseArgs>;
 export type FetchedAssignment = Prisma.AssignmentGetPayload<typeof fetchedAssignmentArgs>;
+
+export type Commit = {
+    files: string[],
+    log: string[],
+}
+
+export type Tier = "S" | "A" | "B" | "C" | "D" | "F";
+export type UserTier = Tier | "?"
+export type TierList = Record<Tier, {name: string, you: boolean}[]>;
