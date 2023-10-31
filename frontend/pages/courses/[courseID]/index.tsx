cdedb380ae4e1b9b77c10f48d990b2ddd2c84ca7
Ido <benhaim.ido@gmail.com>
2023-10-31 02:26:27 -0400

    Integrated courses page

--- frontend/pages/courses/[courseID]/index.tsx
+++ frontend/pages/courses/[courseID]/index.tsx
@@ -1,19 +1,18 @@
-import { CourseSessionChip, AssignmentCard } from '@/components';
+import {AssignmentCard, CourseSessionChip} from '@/components';
 // import { type Course, getCourses } from '@/contexts/UserContext';
 import styles from './page.module.css';
-import { Title2 } from '@fluentui/react-text';
+import {Title2} from '@fluentui/react-text';
+import axios from "@/axios";
+import {FetchedCourseWithTiers} from "codetierlist-types";
+import {useEffect, useState} from "react";
+import {notFound} from "next/navigation";
 // import { notFound } from 'next/navigation';
 
-export default function Page ({ params }: { params: { courseID: string } }) {
-    // const courses = getCourses();
-
-    // let courseObject: Course | undefined;
-
-    // if (!courses.find((course) => course.code === params.courseID)) {
-    //     notFound();
-    // } else {
-    //     courseObject = courses.find((course) => course.code === params.courseID);
-    // }
+export default function Page({params}: { params: { courseID: string } }) {
+    const [course, setCourse] = useState<FetchedCourseWithTiers | null>(null);
+    useEffect(() => {
+        axios.get<FetchedCourseWithTiers>(`/courses/${params.courseID}`, {skipErrorHandling: true}).then((res) => setCourse(res.data)).catch(notFound);
+    }, [params.courseID]);
 
     return (
         <main>
@@ -24,17 +23,13 @@ export default function Page ({ params }: { params: { courseID: string } }) {
                     </CourseSessionChip>
                 </Title2>
                 <Title2>
-                    {/* {courseObject?.name || 'Course not found'} */}
+                    {course?.name || 'Course not found'}
                 </Title2>
             </header>
             <div className="flex-wrap">
-                <AssignmentCard id="1" name="Assignment 1" dueDate={new Date()} tier="S" />
-                <AssignmentCard id="1" name="Assignment 2" dueDate={new Date()} tier="A" />
-                <AssignmentCard id="1" name="Assignment 3" dueDate={new Date()} tier="B" />
-                <AssignmentCard id="1" name="Assignment 4" dueDate={new Date()} tier="C" />
-                <AssignmentCard id="1" name="Assignment 5" dueDate={new Date()} tier="D" />
-                <AssignmentCard id="1" name="Assignment 5" dueDate={new Date()} tier="F" />
-                <AssignmentCard id="1" name="Assignment 6" dueDate={new Date()} tier="?" />
+                {course ? course.assignments.map((assignment) => (
+                    <AssignmentCard key={assignment.title.replaceAll(" ", "_")} id={assignment.title.replaceAll(" ", "_")} name={assignment.title} dueDate={assignment.due_date ?? undefined}  tier={assignment.tier}/>
+                )) : "Loading..."}
             </div>
         </main>
     );
