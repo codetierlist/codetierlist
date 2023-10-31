045caa78b4acf269021f5b22847bad77b647a172
Ido <benhaim.ido@gmail.com>
2023-10-31 01:30:09 -0400

    Some Axios config

--- frontend/axios.ts
+++ frontend/axios.ts
@@ -1,16 +1,10 @@
-// import axios, { InternalAxiosRequestConfig } from 'axios';
-
-declare module 'axios' {
-    export interface AxiosRequestConfig {
-        skipLoadingWheel?: boolean;
-    }
-}
+import axios from "axios";
 
 /**
  * Axios instance
  */
 export const instance = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? '/api' : `${process.env.NEXT_PUBLIC_BACKEND_URL}/`,
+    baseURL: process.env.NODE_ENV === 'production' ? '/api' : `${process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:3030"}/`,
 });
 
 let loading = 0;
@@ -18,52 +12,59 @@ instance.interceptors.request.use((config) => {
     if ('skipLoadingWheel' in config && config.skipLoadingWheel === true) {
         return config;
     }
-    document.getElementById('axios-loading-backdrop').style.display = 'flex';
+    const loadingWheel = document.getElementById('axios-loading-backdrop');
+    if(!loadingWheel){
+        return config;
+    }
+    loadingWheel.style.display = 'flex';
     loading += 1;
     return config;
 });
 
 instance.interceptors.response.use(
     (response) => {
+        const loadingWheel = document.getElementById('axios-loading-backdrop');
+        if(!loadingWheel){
+            return;
+        }
         if ('skipLoadingWheel' in response.config && response.config.skipLoadingWheel === true) {
             return response;
         }
         loading -= 1;
         setTimeout(() => {
             if (loading === 0) {
-                document.getElementById('axios-loading-backdrop').style.display = 'none';
+                loadingWheel.style.display = 'none';
             }
         }, 500);
         return response;
     },
     (error) => {
         loading -= 1;
+        const loadingWheel = document.getElementById('axios-loading-backdrop');
+        if(!loadingWheel){
+            return;
+        }
         setTimeout(() => {
             if (loading === 0) {
-                document.getElementById('axios-loading-backdrop').style.display = 'none';
+                loadingWheel.style.display = 'none';
             }
         }, 500);
         return Promise.reject(error);
     },
 );
-// export const catchAxiosError =
-//     (message: string | undefined, showSnackSev: (message?: string, sev?: AlertColor) => void) => (err: any) => {
-//         if (!err.response) {
-//             showSnackSev('Server did not respond, please open an issue on our Github', 'error');
-//             console.error(err);
-//         } else if (err.response?.status >= 500) {
-//             showSnackSev('Something went wrong, please try again later', 'error');
-//             console.error(err);
-//         } else if (message) {
-//             if (err.response?.data?.message) {
-//                 showSnackSev(`${message}: ${err.response.data.message}`, 'error');
-//             }
-//             showSnackSev(message, 'error');
-//         } else if (err.response?.data?.message) {
-//             showSnackSev(`Something went wrong: ${err.response.data.message}`, 'error');
-//         } else {
-//             showSnackSev('Something went wrong', 'error');
-//         }
-//     };
+const handleError = (message: string) => {
+    // TODO: show error message
+};
+axios.interceptors.response.use((response) => {
+    return response;
+}, error => {
+    if(error.response){
+        if(error.response.config.skipErrorHandling){
+            return Promise.reject(error);
+        }
+        handleError(error.response.data.message);
+    }
+    handleError("Server was unresponsive, please try again later");
+});
 
 export default instance;
