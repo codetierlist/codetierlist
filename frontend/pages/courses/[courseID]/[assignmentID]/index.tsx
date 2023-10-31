b56f211442da52020f6dcb7529e10c7ba894000a
Ido <benhaim.ido@gmail.com>
2023-10-31 14:47:02 -0400

    Added choose files

--- frontend/pages/courses/[courseID]/[assignmentID]/index.tsx
+++ frontend/pages/courses/[courseID]/[assignmentID]/index.tsx
@@ -1,39 +1,43 @@
 // import { CourseSessionChip, AssignmentCard } from '@/components';
 import styles from './page.module.css';
-import { Subtitle2, Title2, Title1 } from '@fluentui/react-text';
+import {Subtitle2, Title1, Title2} from '@fluentui/react-text';
 import Error from 'next/error';
-import { useRouter } from 'next/router';
-import { TierChip, colourHash, TierList } from '@/components';
-import { Button, Card, CardHeader, Title3 } from '@fluentui/react-components';
-import { Add16Regular, Clock16Regular } from '@fluentui/react-icons';
-import { convertDate, convertTime } from '@/components';
-import { CheckedTodoItem } from '@/components/CheckedTodo/CheckedTodo';
+import {useRouter} from 'next/router';
+import {
+    colourHash,
+    convertDate,
+    convertTime,
+    TierChip,
+    TierList
+} from '@/components';
+import {Button, Card, CardHeader, Title3} from '@fluentui/react-components';
+import {Add16Regular, Clock16Regular} from '@fluentui/react-icons';
+import {CheckedTodoItem} from '@/components/CheckedTodo/CheckedTodo';
 import Editor from '@monaco-editor/react';
 import {useEffect, useState} from 'react';
-import {
-    FetchedAssignmentWithTier,
-    Tierlist
-} from "codetierlist-types";
+import {FetchedAssignmentWithTier, Tierlist} from "codetierlist-types";
 import axios from "@/axios";
 import {notFound} from "next/navigation";
 
 // TODO: clean technical debt
 
-const UploadHeader = ({ title, action }: { title: string, action: () => void }) => {
+const UploadHeader = ({title, action}: { title: string, action: () => void }) => {
     return (
         <div className="d-flex justify-content-between">
             <Title1>{title}</Title1>
-            <Button appearance="subtle" onClick={() => action()} icon={<Add16Regular />}>
+            <Button appearance="subtle" onClick={() => action()}
+                icon={<Add16Regular/>}>
                 Upload {title.toLowerCase()}
             </Button>
         </div>
     );
 };
 
-const NoUploadPlaceholder = ({ title }: { title: string }) => {
+const NoUploadPlaceholder = ({title}: { title: string }) => {
     return (
         <>
-            <div className="d-flex flex-column align-items-center justify-content-center h-100">
+            <div
+                className="d-flex flex-column align-items-center justify-content-center h-100">
                 <Title2>
                     Nothing to see here...
                 </Title2>
@@ -44,48 +48,68 @@ const NoUploadPlaceholder = ({ title }: { title: string }) => {
         </>
     );
 };
+const uploader = (url: string, fetchAssignment: ()=>void) => () => {
+    const form = document.createElement('form');
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.name = 'file';
+    form.onformdata = async () => {
+        const formData = new FormData(form);
+        await axios.post<void>(url, formData);
+        document.body.removeChild(form);
+        fetchAssignment();
+    };
+    document.body.append(form);
+    input.click();
 
-const TestUpload = ({ uploadedTests }: { uploadedTests: unknown[] }) => {
+};
+const TestUpload = ({uploadedTests, fetchAssignment}: { uploadedTests: unknown[], fetchAssignment: ()=>void }) => {
+    const router = useRouter();
+    const {courseID, assignmentID} = router.query;
     return (
         <div className="col-12 col-lg-8 mt-4">
-            <UploadHeader title="Test" action={() => { }} />
+            <UploadHeader title="Test" action={() => {
+            }}/>
             <Card className={"mt-4 d-flex flex-column " + styles.editor}>
                 {uploadedTests.length === 0 ? (
-                    <NoUploadPlaceholder title="test" />
+                    <NoUploadPlaceholder title="test"/>
                 ) : (
                     <Editor
                         defaultLanguage="python"
                         defaultValue='sdfasdf'
                         height="90vh"
-                        options={{ readOnly: true }}
+                        options={{readOnly: true}}
                         theme="light"
                     />
                 )}
             </Card>
             <div className="d-flex justify-content-end mt-4">
-                <Button appearance="primary">
+                <Button appearance="primary"
+                    onClick={uploader(`/courses/${courseID}/assignments/${assignmentID}/submissions`, fetchAssignment)}>
                     Submit
                 </Button>
             </div>
         </div>
     );
 };
-
-const SolutionUpload = ({ uploadedSolutions }: { uploadedSolutions: unknown[] }) => {
+const SolutionUpload = ({uploadedSolutions, fetchAssignment}: { uploadedSolutions: unknown[],  fetchAssignment: ()=>void }) => {
+    const router = useRouter();
+    const {courseID, assignmentID} = router.query;
     return (
         <div className="col-12 col-lg-8 mt-4">
-            <UploadHeader title="Solution" action={() => { }} />
+            <UploadHeader title="Solution" action={() => {
+            }}/>
             <Card className={"mt-4 d-flex flex-column " + styles.editor}>
                 {
                     uploadedSolutions.length === 0 ? (
-                        <NoUploadPlaceholder title="solution" />
+                        <NoUploadPlaceholder title="solution"/>
                     ) :
                         (
                             <Editor
                                 defaultLanguage="python"
                                 defaultValue='sdfasdf'
                                 height="90vh"
-                                options={{ readOnly: true }}
+                                options={{readOnly: true}}
                                 theme="light"
                             />
                         )
@@ -93,7 +117,8 @@ const SolutionUpload = ({ uploadedSolutions }: { uploadedSolutions: unknown[] })
 
             </Card>
             <div className="d-flex justify-content-end mt-4">
-                <Button appearance="primary">
+                <Button appearance="primary"
+                    onClick={()=>{console.log("test");uploader(`/courses/${courseID}/assignments/${assignmentID}/submissions`, fetchAssignment)();}}>
                     Submit
                 </Button>
             </div>
@@ -101,7 +126,7 @@ const SolutionUpload = ({ uploadedSolutions }: { uploadedSolutions: unknown[] })
     );
 };
 
-const ViewTierList = ({tierlist}: {tierlist: Tierlist}) => {
+const ViewTierList = ({tierlist}: { tierlist: Tierlist }) => {
     return (
         <div className="col-12 col-lg-8 mt-4">
             <Title2>
@@ -121,7 +146,7 @@ export default function Page() {
     const [tierlist, setTierlist] = useState<Tierlist | null>(null);
 
     // TODO: guard against invalid courseID, invalid assignmentID
-    const { courseID, assignmentID } = router.query;
+    const {courseID, assignmentID} = router.query;
 
     const fetchAssignment = async () => {
         await axios.get<FetchedAssignmentWithTier>(`/courses/${courseID}/assignments/${assignmentID}`, {skipErrorHandling: true}).then((res) => setAssignment(res.data)).catch(e => {
@@ -144,20 +169,22 @@ export default function Page() {
     }, [courseID, assignmentID]);
 
     if (!courseID || !assignmentID) {
-        return <Error statusCode={404} />;
+        return <Error statusCode={404}/>;
     }
-    if(!assignment) {
+    if (!assignment) {
         return <p>Loading...</p>;
     }
     return (
         <main className="container-fluid">
             <Card className={styles.header}>
                 <div className={"d-flex justify-content-between"}>
-                    <div className={styles.assignmentHeaderContent + " flex-column d-flex"}>
+                    <div
+                        className={styles.assignmentHeaderContent + " flex-column d-flex"}>
                         <CardHeader
                             header={
                                 <>
-                                    <Clock16Regular className={styles.dueDateIcon} />
+                                    <Clock16Regular
+                                        className={styles.dueDateIcon}/>
                                     <Subtitle2 className={styles.dueDate}>
                                         Due {convertDate(assignment.due_date)} at {convertTime(assignment.due_date)}
                                     </Subtitle2>
@@ -165,14 +192,15 @@ export default function Page() {
                             }
                         />
                         <Title2>
-                            <span className={colourHash(courseID as string) + ' ' + styles.courseCode}>
+                            <span
+                                className={colourHash(courseID as string) + ' ' + styles.courseCode}>
                                 {courseID}
                             </span>
                             {assignmentID}
                         </Title2>
                     </div>
                     <div>
-                        <TierChip tier={assignment.tier} />
+                        <TierChip tier={assignment.tier}/>
                     </div>
                 </div>
             </Card>
@@ -184,7 +212,8 @@ export default function Page() {
                 <Button className="mx-2" onClick={() => setStage(2)}>
                     Upload a solution
                 </Button>
-                <Button disabled={assignment.tier === "?"} onClick={() => setStage(3)}>
+                <Button disabled={assignment.tier === "?"}
+                    onClick={() => setStage(3)}>
                     View tier list
                 </Button>
             </div>
@@ -196,14 +225,21 @@ export default function Page() {
                             header={<Title3>Status</Title3>}
                         />
                         <div className="d-flex flex-column">
-                            <Button onClick={() => setStage(1)} appearance="subtle" className="d-block">
-                                <CheckedTodoItem todo="Submit a valid test" checked={assignment.test_cases.length > 0} />
+                            <Button onClick={() => setStage(1)}
+                                appearance="subtle" className="d-block">
+                                <CheckedTodoItem todo="Submit a valid test"
+                                    checked={assignment.test_cases.length > 0}/>
                             </Button>
-                            <Button onClick={() => setStage(2)} appearance="subtle" className="d-block">
-                                <CheckedTodoItem todo="Submit your code" checked={assignment.submissions.length > 0} />
+                            <Button onClick={() => setStage(2)}
+                                appearance="subtle" className="d-block">
+                                <CheckedTodoItem todo="Submit your code"
+                                    checked={assignment.submissions.length > 0}/>
                             </Button>
-                            <Button onClick={() => setStage(3)} appearance="subtle" className="d-block" disabled={assignment.tier === "?"}>
-                                <CheckedTodoItem todo="View tier list" checked={assignment.tier !== "?"} />
+                            <Button onClick={() => setStage(3)}
+                                appearance="subtle" className="d-block"
+                                disabled={assignment.tier === "?"}>
+                                <CheckedTodoItem todo="View tier list"
+                                    checked={assignment.tier !== "?"}/>
                             </Button>
                         </div>
                     </Card>
@@ -220,7 +256,8 @@ export default function Page() {
                         <ul className={"d-flex flex-column " + styles.uploadedTests}>
                             {assignment.test_cases.map((test, index) => (
                                 // TODO this is not very informative
-                                <li className={styles.uploadedTest} key={index}>{test.git_id}</li>
+                                <li className={styles.uploadedTest}
+                                    key={index}>{test.git_id}</li>
                             ))}
                         </ul>
                     </Card>
@@ -236,25 +273,27 @@ export default function Page() {
                         }
                         <ul className={"d-flex flex-column " + styles.uploadedSolutions}>
                             {assignment.submissions.map((test, index) => (
-                                <li className={styles.uploadedTest} key={index}>{test.git_id}</li>
+                                <li className={styles.uploadedTest}
+                                    key={index}>{test.git_id}</li>
                             ))}
                         </ul>
                     </Card>
                 </div>
                 {
                     stage === 1 && (
-                        <TestUpload uploadedTests={assignment.test_cases} />
+                        <TestUpload uploadedTests={assignment.test_cases} fetchAssignment={fetchAssignment}/>
                     )
                 }{
                     stage === 2 && (
-                        <SolutionUpload uploadedSolutions={assignment.submissions} />
+                        <SolutionUpload uploadedSolutions={assignment.submissions} fetchAssignment={fetchAssignment}/>
                     )
                 }{
                     stage === 3 && (
-                        tierlist ? <ViewTierList tierlist={tierlist}/> : "No tierlist found"
+                        tierlist ?
+                            <ViewTierList tierlist={tierlist}/> : "No tierlist found"
                     )
                 }
             </div>
-        </main >
+        </main>
     );
 }
