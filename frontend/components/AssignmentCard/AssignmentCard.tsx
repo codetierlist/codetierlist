418d402d04531bfdf251d156b895c33474efd563
Ido <benhaim.ido@gmail.com>
2023-10-31 02:28:11 -0400

    Fixed assignmentcard

--- frontend/components/AssignmentCard/AssignmentCard.tsx
+++ frontend/components/AssignmentCard/AssignmentCard.tsx
@@ -1,25 +1,37 @@
-import { Caption1, Card, CardHeader, Title3, Link } from '@fluentui/react-components';
+import {
+    Caption1,
+    Card,
+    CardHeader,
+    Link,
+    Title3
+} from '@fluentui/react-components';
 import styles from './AssignmentCard.module.css';
-import { TierChip } from '@/components';
-import { type Tier, type UserTier } from "codetierlist-types";
+import {TierChip} from '@/components';
+import {type Tier, type UserTier} from "codetierlist-types";
 
 export declare interface AssignmentCardProps {
-  id: string
-  name: string
-  dueDate: Date
-  tier: UserTier | Tier
+    id: string
+    name: string
+    dueDate?: Date
+    tier: UserTier | Tier
 }
 
-export const AssignmentCard = ({ id, name, dueDate, tier }: AssignmentCardProps): JSX.Element => {
+export const AssignmentCard = ({
+    id,
+    name,
+    dueDate,
+    tier
+}: AssignmentCardProps): JSX.Element => {
     return (
         <Link href={`/assignments/${id}`} className={styles.cardLink}>
             <Card className={styles.card}>
                 <CardHeader
-                    header={(<TierChip tier={tier} />)}
+                    header={(<TierChip tier={tier}/>)}
                 >
                 </CardHeader>
                 <div className={styles.cardContent}>
-                    <Caption1>{ dueDate.toLocaleDateString() }</Caption1>
+                    {dueDate ?
+                        <Caption1>{dueDate.toLocaleDateString()}</Caption1> : null}
                     <Title3>{name}</Title3>
                 </div>
             </Card>
