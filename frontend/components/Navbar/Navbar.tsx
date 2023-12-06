7e6fd6e3b1db0cab089e3fc122fde335b158c304
logonoff <logonoff@users.noreply.github.com>
2023-12-06 01:06:02 -0500

    add popover

--- frontend/components/Navbar/Navbar.tsx
+++ frontend/components/Navbar/Navbar.tsx
@@ -1,9 +1,75 @@
 import { GenerateInitalsAvatarProps, generateInitals } from '@/components';
 import { UserContext, defaultUser } from '@/contexts/UserContext';
-import { Badge, Persona, SkeletonItem } from '@fluentui/react-components';
+import { Button, Badge, Persona, SkeletonItem, Popover, Switch, PopoverSurface, PopoverTrigger } from '@fluentui/react-components';
 import Link from 'next/link';
-import { useContext } from 'react';
+import { useContext, useState, useCallback } from 'react';
 import styles from './Navbar.module.css';
+
+const UserPopoverContent = (): JSX.Element => {
+    const { userInfo } = useContext(UserContext);
+    const [checked, setChecked] = useState(true);
+    const onChange = useCallback(
+        (ev) => {
+            setChecked(ev.currentTarget.checked);
+        },
+        [setChecked]
+    );
+
+    return (
+        <div>
+            <div className={styles.popoverButtonContainer}>
+                <Switch
+                    checked={checked}
+                    onChange={onChange}
+                    label={checked ? "Dark mode" : "Light mode"}
+                    className={styles}
+                />
+
+                <Link href="">
+                    <Button appearance="subtle">
+                        Sign out
+                    </Button>
+                </Link>
+            </div>
+
+            <Persona
+                size="huge"
+                className={styles.popoverPersona}
+                avatar={GenerateInitalsAvatarProps(generateInitals(userInfo))}
+                primaryText={
+                    <>
+                        {`${userInfo.givenName} ${userInfo.surname}` == " " ? userInfo.utorid : `${userInfo.givenName} ${userInfo.surname}`}
+                        {userInfo.admin && <Badge className={styles.adminBadge} appearance="outline">Admin</Badge>}
+                    </>
+                }
+                secondaryText={userInfo.email}
+                tertiaryText={userInfo.utorid}
+            />
+        </div>
+    );
+};
+
+const UserAvatar = (): JSX.Element => {
+    const { userInfo } = useContext(UserContext);
+
+    return (
+        <Persona
+            textPosition="before"
+            avatar={GenerateInitalsAvatarProps(generateInitals(userInfo))}
+            primaryText={
+                <span className={styles.subtext}>
+                    {userInfo.admin && <Badge className={styles.adminBadge} appearance="outline">Admin</Badge>}
+                    {`${userInfo.givenName} ${userInfo.surname}` == " " ? userInfo.utorid : `${userInfo.givenName} ${userInfo.surname}`}
+                </span>
+            }
+            secondaryText={
+                <span className={styles.subtext}>
+                    { userInfo.utorid }
+                </span>
+            }
+        />
+    );
+};
 
 /**
  * The navbar component is the top bar of the website. It contains the
@@ -20,21 +86,16 @@ export const Navbar = (): JSX.Element => {
             </h1>
 
             {(userInfo.email !== defaultUser.email) && (
-                <Persona
-                    textPosition="before"
-                    avatar={GenerateInitalsAvatarProps(generateInitals(userInfo))}
-                    primaryText={
-                        <span className={styles.subtext}>
-                            {userInfo.admin && <Badge className={styles.adminBadge} appearance="outline">Admin</Badge>}
-                            {`${userInfo.givenName} ${userInfo.surname}` == " " ? userInfo.utorid : `${userInfo.givenName} ${userInfo.surname}`}
-                        </span>
-                    }
-                    secondaryText={
-                        <span className={styles.subtext}>
-                            { userInfo.utorid }
-                        </span>
-                    }
-                />
+                <Popover>
+                    <PopoverTrigger>
+                        <Button appearance="subtle" size="small" className={styles.userButton}>
+                            <UserAvatar />
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverSurface>
+                        <UserPopoverContent />
+                    </PopoverSurface>
+                </Popover>
             )}
 
             {(userInfo.email === defaultUser.email) && (
@@ -46,6 +107,7 @@ export const Navbar = (): JSX.Element => {
                     <SkeletonItem shape="circle" size={36} />
                 </div>
             )}
+
         </header>
     );
 };
