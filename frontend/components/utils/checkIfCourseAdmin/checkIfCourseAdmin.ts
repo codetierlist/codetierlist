c5d2bcade0ac0c855732ef1f689dca1f683154ea
logonoff <logonoff@users.noreply.github.com>
2023-12-13 13:32:10 -0500

    Update checkIfCourseAdmin.ts

--- frontend/components/utils/checkIfCourseAdmin/checkIfCourseAdmin.ts
+++ frontend/components/utils/checkIfCourseAdmin/checkIfCourseAdmin.ts
@@ -1,7 +1,11 @@
 import { FetchedUser } from 'codetierlist-types';
 
 /**
- * Returns true if the user is an admin or has a role in the course
+ * Check if a user has administrator privileges in a course
+ *
+ * @param user The user to check
+ * @param courseId The course to check
+ * @returns true if the user is an admin or has a role in the course
  */
 export const checkIfCourseAdmin = (user: FetchedUser, courseId: string) => {
     return user.roles.some((role) => role.course_id === courseId) || user.admin;
