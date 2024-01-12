6b9f64837bc13df577e475820986b7dffbbf9eab
logonoff <18614559+logonoff@users.noreply.github.com>
2024-01-12 10:49:40 -0500

    overflow avatars when over 20

--- frontend/components/TierList/TierList.tsx
+++ frontend/components/TierList/TierList.tsx
@@ -1,6 +1,5 @@
-import { AvatarGroup, AvatarGroupItem } from "@fluentui/react-components";
+import { AvatarGroup, AvatarGroupItem, AvatarGroupPopover, partitionAvatarGroupItems } from "@fluentui/react-components";
 import { Tier, Tierlist, UserTier } from "codetierlist-types";
-import { Fragment } from "react";
 import { Col, Row } from "react-grid-system";
 import { TierChip } from "..";
 import { GenerateInitalsAvatarProps } from "../../components/InitialsAvatar/InitialsAvatar";
@@ -60,52 +59,97 @@ const Hardcode = [
 }, {} as Tierlist);
 
 /**
+ * A tier indicator displays the tier of the tier list.
+ * @property {Tier} tier the tier to display
+ */
+const TierIndicator = ({ tier }: { tier: Tier }): JSX.Element => {
+    return (
+        <Col
+            style={{
+                textAlign: "center",
+                padding: 0
+            }}
+            sm={12} lg={2}
+        >
+            <TierChip
+                tier={tier as UserTier}
+                className={`py-2 px-0 ${styles.tier}`}
+            />
+        </Col>
+    );
+};
+
+/**
+ * A tier avatars displays the avatars of the people in the tier.
+ * @property {string[]} people the people to display
+ * @property {number} maxInlineItems the maximum number of people to display before showing a +x
+ */
+const TierAvatars = ({ people, maxInlineItems }: { people: { name: string, you: boolean }[], maxInlineItems: number }): JSX.Element => {
+    const { inlineItems, overflowItems } = partitionAvatarGroupItems({ items: people, maxInlineItems });
+
+    return (
+        <Col
+            style={{
+                backgroundColor: "var(--colorNeutralBackground1Hover)",
+                borderBottom: "1px solid var(--colorNeutralBackground1Pressed)",
+            }}
+            sm={12} lg={10}
+        >
+            <AvatarGroup className={styles.avatarGroup}>
+                {
+                    inlineItems.map((person, i) => {
+                        return (
+                            <AvatarGroupItem
+                                key={i}
+                                className={person.you ? `${styles.you} ${styles.avatar}` : styles.avatar}
+                                {...GenerateInitalsAvatarProps(person.name)}
+                            />
+                        );
+                    })
+                }
+                {
+                    overflowItems && (
+                        <AvatarGroupItem
+                            overflowLabel={`${overflowItems.length} more`}
+                            className={styles.avatar}
+                            initials={`+${overflowItems.length}`}
+                            color={"neutral"}
+                        />
+                    )
+                }
+            </AvatarGroup>
+        </Col>
+    );
+};
+
+/**
+ * A tier displays a tier and the people in the tier.
+ * @property {string} tier the tier to display
+ * @property {Tierlist} tierlist the tierlist to display
+ */
+const TierRow = ({ tier, tierlist }: { tier: string, tierlist: Tierlist }): JSX.Element => {
+    return (
+        <>
+            <TierIndicator tier={tier as Tier} />
+
+            <TierAvatars people={tierlist[tier as Tier]} maxInlineItems={20} />
+        </>
+    );
+};
+
+/**
  * A tier list displays a list of people in a tier list format.
  * @property {Tierlist} tierlist the tierlist to display
  * @returns {JSX.Element} the tier list
  */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const TierList = ({ tierlist = Hardcode }: { tierlist: Tierlist }): JSX.Element => {
     return (
         <Row component="section">
             {
-                Object.keys(tierlist).map((tier, index) => {
+                Object.keys(Hardcode).map((tier, index) => {
                     return (
-                        <Fragment key={index}>
-                            <Col
-                                style={{
-                                    textAlign: "center",
-                                    padding: 0
-                                }}
-                                sm={12} lg={2}
-                            >
-                                <TierChip
-                                    tier={tier as UserTier}
-                                    className={`py-2 px-0 ${styles.tier}`}
-                                />
-                            </Col>
-
-                            <Col
-                                style={{
-                                    backgroundColor: "var(--colorNeutralBackground1Hover)",
-                                    borderBottom: "1px solid var(--colorNeutralBackground1Pressed)",
-                                }}
-                                sm={12} lg={10}
-                            >
-                                <AvatarGroup className={styles.avatarGroup}>
-                                    {
-                                        tierlist[tier as Tier].map((person, i) => {
-                                            return (
-                                                <AvatarGroupItem
-                                                    key={i}
-                                                    className={person.you ? `${styles.you} ${styles.avatar}` : styles.avatar}
-                                                    {...GenerateInitalsAvatarProps(person.name)}
-                                                />
-                                            );
-                                        })
-                                    }
-                                </AvatarGroup>
-                            </Col>
-                        </Fragment>
+                        <TierRow key={index} tier={tier} tierlist={Hardcode} />
                     );
                 })
             }
