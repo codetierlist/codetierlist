b9454119d4d3de76bc5a74ba433cbb9ba231e921
Ido <benhaim.ido@gmail.com>
2024-02-03 15:52:12 -0500

    Nested parent jobs

--- types/index.d.ts
+++ types/index.d.ts
@@ -119,16 +119,25 @@ type JobFiles = {
     [key: string]: string
 }
 
-export type JobData = {
+export type ReadyJobData = {
     image: RunnerImage,
     testCase: TestCase,
     submission: Submission,
     query: { solution_files: JobFiles, test_case_files: JobFiles }
+} | {status: "WAITING_FILES"}
+
+export type PendingJobData = {
+    image: RunnerImage,
+    testCase: TestCase,
+    submission: Submission,
 }
 export type ParentJobData = {
     item: Submission | TestCase,
-    type: "submission" | "testcase"
+    type: "submission" | "testcase",
+    status: ParentJobStatus
 }
+
+export type ParentJobStatus = "WAITING_FILES" | "READY" | "COMPLETED"
 
 enum _JobStatus {
     PASS = "PASS", // passes all test cases
