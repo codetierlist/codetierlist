0a848496e20bb876c859cc98bdf00a3e72efb093
Ido <benhaim.ido@gmail.com>
2024-02-03 15:26:40 -0500

    Added more debug info

--- backend/src/common/runner.ts
+++ backend/src/common/runner.ts
@@ -189,6 +189,7 @@ export const removeTestcases = async (utorid: string): Promise<void> => {
 
 new Worker<ParentJobData, undefined, JobType>(parent_job_queue, async (job, token) => {
     if (!job || !job.data) return;
+    console.info(`Parent job ${job.id} started processing at ${Date.now()}`);
     while(job.data.status !== "COMPLETED"){
         if (token) {
             const shouldWait = await job.moveToWaitingChildren(token);
