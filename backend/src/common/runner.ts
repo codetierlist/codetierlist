8ef4b250112bfa5f32ac3b788e98fb244d1166cc
Ido <benhaim.ido@gmail.com>
2024-02-03 18:16:02 -0500

    remove children

--- backend/src/common/runner.ts
+++ backend/src/common/runner.ts
@@ -240,7 +240,9 @@ new Worker<ParentJobData, undefined, JobType>(parent_job_queue, async (job, toke
             number: passedOrFailed.map(x => "amount" in x ? x.amount : 0).reduce((a, b) => a + b, 0) / passedOrFailed.length
         });
     }
-    await job.remove();
+    await job.remove({
+        removeChildren: true
+    });
     console.info(`Parent job ${job.id} completed with ${passed.length} passed out of ${children.length}. Finished processing at ${Date.now()}`);
     return undefined;
 }, queue_conf);
