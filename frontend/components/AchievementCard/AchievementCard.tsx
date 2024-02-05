38c003f237a8a53c5897c08b2817ef175964c3a5
logonoff <18614559+logonoff@users.noreply.github.com>
2024-02-04 20:02:22 -0500

    redesign acheivements

--- frontend/components/AchievementCard/AchievementCard.tsx
+++ frontend/components/AchievementCard/AchievementCard.tsx
@@ -1,13 +1,28 @@
-import {AchievementConfig} from "codetierlist-types";
-import {ToolTipIcon} from "@/components";
+import {
+    Caption1,
+    Card,
+    CardPreview,
+    Text
+} from "@fluentui/react-components";
+import { AchievementConfig } from "codetierlist-types";
 
-export const AchievementCard = ({achievement}: { achievement: AchievementConfig }) => {
+export const AchievementCard = ({ achievement }: { achievement: AchievementConfig }) => {
     return (
-        <ToolTipIcon tooltip={(<><h3>{achievement.name}</h3><br/>
-            <p>{achievement.description}</p></>)}
-        icon={<img src={`achievements/${achievement.icon}`} alt={achievement.name}
-            // TODO make this scale better
-            style={{width: "100px", height: "100px", borderRadius: "100%"}}/>}
-        className={"achievementCard"}/>
+        <Card orientation="horizontal">
+            <CardPreview>
+                <img
+                    src={`achievements/${achievement.icon}`}
+                    alt=""
+                />
+            </CardPreview>
+
+            <div>
+                <Text as="h3" weight="semibold">{achievement.name}</Text>
+                <br />
+                <Caption1 as="p">
+                    {achievement.description}
+                </Caption1>
+            </div>
+        </Card>
     );
 };
--- frontend/pages/achievements/index.tsx
+++ frontend/pages/achievements/index.tsx
@@ -1,24 +1,32 @@
 import axios, { handleError } from "@/axios";
+import { AchievementCard } from "@/components";
+import { SnackbarContext } from '@/contexts/SnackbarContext';
+import { UserContext } from "@/contexts/UserContext";
 import {
-    Caption1
+    Caption1,
+    Card,
+    CardPreview,
+    Text
 } from "@fluentui/react-components";
 import { Title2 } from '@fluentui/react-text';
-import {AchievementConfig} from "codetierlist-types";
+import { AchievementConfig } from "codetierlist-types";
 import { notFound } from "next/navigation";
-import { useContext, useEffect, useState } from "react";
-import { Container } from "react-grid-system";
-import { SnackbarContext } from '@/contexts/SnackbarContext';
-import {AchievementCard} from "@/components";
-import {UserContext} from "@/contexts/UserContext";
+import { useContext, useEffect, useMemo, useState } from "react";
+import { Col, Container, Row } from "react-grid-system";
 
 export default function Page() {
     const [achievements, setAchievements] = useState<AchievementConfig[] | null>(null);
     const { showSnackSev } = useContext(SnackbarContext);
-    const { userInfo,  setUserInfo} = useContext(UserContext);
+    const { userInfo, setUserInfo } = useContext(UserContext);
+    const hiddenAchievementCount = useMemo(() => {
+        if (!achievements) return 0;
+
+        return achievements.filter(achievement => achievement.id === -1).length;
+    }, [achievements]);
 
     const fetchAchievements = async () => {
         await axios.get<AchievementConfig[]>(`/users/achievements`, { skipErrorHandling: true })
-            .then((res) => {setAchievements(res.data); setUserInfo({...userInfo, new_achievements: false});})
+            .then((res) => { setAchievements(res.data); setUserInfo({ ...userInfo, new_achievements: false }); })
             .catch(e => {
                 handleError(showSnackSev)(e);
                 notFound();
@@ -27,6 +35,7 @@ export default function Page() {
 
     useEffect(() => {
         void fetchAchievements();
+
         document.title = `Achievements - Codetierlist`;
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
@@ -34,12 +43,39 @@ export default function Page() {
     return (
         <Container component="main" className="m-t-xxxl">
             <header className={"m-b-l"}>
-                <Title2>Achievements</Title2><br/>
-                <Caption1>Your achievements will show here!</Caption1>
+                <Title2 as="h2">Achievements</Title2><br />
             </header>
-            <div className="flex-wrap">
-                {achievements?.map(achievement => <AchievementCard key={achievement.id} achievement={achievement} />)}
-            </div>
+            <Row>
+                {
+                    achievements?.filter(achievement => achievement.id !== -1)
+                        .map(
+                            achievement =>
+                                <Col key={achievement.id} sm={12} className="m-b-l">
+                                    <AchievementCard key={achievement.id} achievement={achievement} />
+                                </Col>
+                        )
+                }
+            </Row>
+
+            {hiddenAchievementCount > 0 &&
+                <Row>
+                    <Col sm={12} className="m-b-l">
+                        <Card orientation="horizontal">
+                            <CardPreview>
+                                <img src={`achievements/unknown.png`} alt="" />
+                            </CardPreview>
+
+                            <div>
+                                <Text as="h3" weight="semibold">{hiddenAchievementCount} hidden achievement{hiddenAchievementCount > 1 ? "s" : ""} remaining</Text>
+                                <br/>
+                                <Caption1 as="p">
+                                    Details of these achievements are hidden until you unlock them.
+                                </Caption1>
+                            </div>
+                        </Card>
+                    </Col>
+                </Row>
+            }
         </Container>
     );
 }
