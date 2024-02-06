83f540d09011bea7016b821d2329c0ff65757a63
logonoff <18614559+logonoff@users.noreply.github.com>
2024-02-06 03:50:03 -0500

    trigger rebuild

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import prisma from "./common/prisma";
 import "./common/achievements/events";
+
 process.on("unhandledRejection", e=>{
     console.error(e);
 });
+
 process.on("uncaughtException", e=>{
     console.error(e);
 });
+
 prisma.$connect().then(async () => {
     console.info("Connected to database");
     console.info("Starting server");
