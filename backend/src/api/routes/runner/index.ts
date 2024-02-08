c72cea31aa335f3e0ce1152cfc0fa1bfd7623510
logonoff <git@logonoff.co>
2024-02-08 11:31:57 -0500

    authenticate images route

--- backend/src/api/routes/runner/index.ts
+++ backend/src/api/routes/runner/index.ts
@@ -3,14 +3,21 @@ import {
 } from "codetierlist-types";
 import { images } from "../../../common/config";
 import express from "express";
-
+import {
+    RoleType
+} from "@prisma/client";
 const router = express.Router();
 
 /**
  * Get the images for the runner
- * @public
+ * @adminonly
  */
-router.get("/images", (_, res) =>{
+router.get("/images", (req, res) =>{
+    if (!req.user.admin && !req.user.roles.some((role: RoleType) =>
+        role === RoleType.INSTRUCTOR || role === RoleType.TA)) {
+        return res.status(403).send("You must be a TA or Instructor of at least one course to access this route");
+    }
+
     res.send(images satisfies RunnerImage[]);
 });
 
