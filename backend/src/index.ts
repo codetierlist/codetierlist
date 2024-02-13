bce2a23544e2db6c0aa9315de4183fe0dd6e477e
logonoff <git@logonoff.co>
2024-02-13 01:42:35 -0500

    Add graceful shutdown handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,3 +14,16 @@ prisma.$connect().then(async () => {
     console.info("Starting server");
     await import("./api");
 });
+
+// trap SIGINT and SIGTERM and gracefully shutdown
+process.on("SIGINT", async () => {
+    console.info("Stopping server");
+    await prisma.$disconnect();
+    process.exit(0);
+});
+
+process.on("SIGTERM", async () => {
+    console.info("Stopping server");
+    await prisma.$disconnect();
+    process.exit(0);
+});
