e7e6cac37be056ef8a02d297935127cba24b9d10
logonoff <git@logonoff.co>
2024-02-13 02:56:30 -0500

    redis.sh: enforce REDIS_PASSWORD

--- redis/redis.sh
+++ redis/redis.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-exec redis-server /usr/local/etc/redis/redis.conf --requirepass $REDIS_PASSWORD
+exec redis-server /usr/local/etc/redis/redis.conf --requirepass ${REDIS_PASSWORD?you forgot to add REDIS_PASSWORD to the environment}
+```
