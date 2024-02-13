4210d82e5970e78fe6b0a9c1ce1b89209d542f19
logonoff <git@logonoff.co>
2024-02-13 03:22:11 -0500

    Require REDIS_PASSWORD to be non-empty

--- redis/redis.sh
+++ redis/redis.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-exec redis-server /usr/local/etc/redis/redis.conf --requirepass ${REDIS_PASSWORD?you forgot to add REDIS_PASSWORD to the environment}
+exec redis-server /usr/local/etc/redis/redis.conf --requirepass ${REDIS_PASSWORD:?you forgot to add REDIS_PASSWORD to the environment}
