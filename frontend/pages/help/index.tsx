c550498ae1488d408072966af9ad66fc6ac8debe
logonoff <18614559+logonoff@users.noreply.github.com>
2024-02-25 00:08:03 -0500

    added help page

--- frontend/pages/help/index.tsx
+++ frontend/pages/help/index.tsx
@@ -1,12 +1,18 @@
+import { Caption1, LargeTitle, Subtitle1 } from "@fluentui/react-text";
 import { Container } from "react-grid-system";
+import styles from './help.module.css';
 
 export const HelpPage = (): JSX.Element => {
     return (
         <Container component="main" className="m-t-xxxl">
-            <h1>Help</h1>
-            <p>
-                This is the help page. It will contain information about how to use the website.
-            </p>
+            <header className={styles.header}>
+                <LargeTitle>Help</LargeTitle>
+                <Caption1>
+                    This page is for support and documentation regarding Codetierlist. For help
+                    with your assignments, please contact your instructor.
+                </Caption1>
+            </header>
+
         </Container>
     );
 }
