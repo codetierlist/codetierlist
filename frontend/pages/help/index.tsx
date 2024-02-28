ff35ca8a78b0e2ce1ae7856df476086504b37c64
Ido <benhaim.ido@gmail.com>
2024-02-28 02:32:20 -0500

    Dynamically load help markdown

--- frontend/pages/help/index.tsx
+++ frontend/pages/help/index.tsx
@@ -18,89 +18,12 @@ import Head from 'next/head';
 import styles from './help.module.css';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import React, { Attributes, FunctionComponent, ReactNode } from 'react';
-
-const markdown = `# Codetierlist Full Student Guide
-This guide will help you get started with the Codetierlist as a student.
-## Table of Contents
-- [Testing in Software Development](#testing-in-software-development)
-- [What is Codetierlist?](#what-is-codetierlist)
-- [Getting Started](#getting-started)
-- [Courses](#courses)
-  - [Enrollment](#enrollment)
-- [Assignments](#assignments)
-  - [Submission](#submission)
-    - [Code](#code)
-    - [Test Cases](#test-cases) 
-  - [Tierlist](#tierlist)
-  - [Due Dates](#due-dates)
-- [Data Privacy](#data-privacy)
-- [Contact and Support](#contact-and-support)
-
-## Testing in Software Development
-Testing is a major part of the software development process. Many development models like Agile and Waterfall include testing as a key part of the development process. There are many ways to develop testcases alongside the code, Codetierlist is inspired by ping-pong pair programing where the testcases are developed alongside the code by a fellow peer. Ping-pong pair programming is a technique where one peer programs and the other peer writes the testcases. This is a great way to develop testcases as the person writing the testcases is not the person who wrote the code, so they are less likely to be biased towards the code.
-
-## What is Codetierlist?
-Ever wondered when you complete an assignment and do not know how well you're doing compared to other students? Introducing the Codetierlist! We provide students an opportunity to test their code and know how they compare to their peers, without sharing the complete testcases. This makes coding and testing more interactive, fun, and easier.
-
-On Codetierlist, instructors can create individual assignments for students to complete. In each assignment, students are able to upload their own test cases and code. How well students do will be based on the accuracy of the students' code in their own test cases. This will be shown in a tier list for students to see.
-
-## Getting Started
-To get started go to https://codetierlist.utm.utoronto.ca/ and sign in with your UofT credentials. Once you are signed in, you will be able to see the courses you are enrolled in.
-> [!NOTE]
-> If you are not enrolled in any courses, you will not be able to see any courses on the home page. If you think you should be enrolled in a course, please contact your instructors.
-
-## Courses
-Each course will have its own page where you can view individual assignments for the course. Clicking on a course you will see all assignments (past and present) for the course.
-
-### Enrollment
-You will be automatically enrolled in a course by your instructor. If you are not enrolled in a course, please contact your instructor.
-
-## Assignments
-From the course page, you can select individual assignment to view. Each assignment will have its own page where you can view the tier list and submit your code and test cases.
-
-### Submission
-You can submit your code and test cases for an assignment by navigating to the "upload" tab on the assignment page. You can submit your code and test cases by clicking on the upload button and selecting the file you want to upload.
-You can submit multiple files at once by selecting multiple files in the file picker.
-> [!NOTE]
-> Alternatively, you can drag and drop the files into the upload area.
-
-#### Code
-Your code should be uploaded under the "uploaded solutions" sections. Make sure you upload every file required for the assignment to run properly. If you are unsure of what files are required, please contact your instructor.
-
-#### Test Cases
-Your test cases should be uploaded under the "uploaded test cases" section. Make sure you upload every file required for the test cases to run properly. If you are unsure of what files are required, please contact your instructor.
-
-### Tierlist
-After you submit **both** your code and your test cases, you will be able to view the tier list for the assignment. The tier list will show you how well you did compared to your peers. The tier list will be anonymized, so you will not be able to see who submitted what code and test cases. The tier list will be updated every time a new submission is made.
-The tierlist is dynamic and will update if your classmates update their code and test cases. This means that you can see how well you are doing compared to your peers in real-time.
-
-> [!NOTE]
-> The tierlist may not be updated immediately after you submit your code and test cases. Depending on the system load, it may take a few minutes for the tier list to update with your submission.
-
-> [!WARNING]
-> The tierlist does not reflect the accuracy of your code in the instructor's test cases (or final mark). The tierlist only reflects the accuracy of your code in your test cases.
-> The tierlist is only as good as the testcases submitted by your peers. If the testcases are not comprehensive, the tierlist may not accurately reflect the accuracy of your code. So make sure to submit comprehensive test cases!
-
-### Due Dates
-Each assignment will have a due date. The due date will be displayed on the assignment page. Make sure to submit your code and test cases before the due date. Late submissions will not be accepted unless stated otherwise.
-> [!NOTE]
-> Instructors may choose to allow late submissions. If you are unsure of the late submission policy, please contact your instructor.
-
-## Data Privacy
-Anything you upload to Codetierlist will be visible to the instructors and TAs of the course, however, your uploads will not be used for grading the assignment. 
-The tierlist will be anonymized to students, so your peers will not be able to see who submitted what code and test cases.
-> [!WARNING]
-> Only submit your own code and test cases. Do not share your code and test cases with your peers. Sharing your code and test cases with your peers may be considered academic dishonesty.
-> If you are unsure of what is considered academic dishonesty, please contact your instructor.
-
-## Contact and Support
-If you have any questions about Codetierlist, please contact any of the following people:
-- Ido Ben Haim ([ido.benhaim@mail.utoronto.ca](mailto:ido.benhaim@mail.utoronto.ca))
-- Daksh Malhotra ([d.malhotra@utoronto.ca](mailto:d.malhotra@utoronto.ca))
-- Jackson Lee ([jacks.lee@utoronto.ca](mailto:jacks.lee@utoronto.ca))
-
-If you have any questions about a specific course or assignment, please contact your instructor or TA. You can find their contact information on the course page.`;
+import React, {
+    Attributes,
+    FunctionComponent,
+    ReactNode,
+    useEffect, useState
+} from 'react';
 
 const getNodeText = (node: React.ReactNode): string => {
     if (['string', 'number'].includes(typeof node)) return (node || '').toString();
@@ -202,6 +125,12 @@ const Quote = ({ children }: { children: ReactNode }) => {
     return <blockquote>{children}</blockquote>;
 };
 export const HelpPage = (): JSX.Element => {
+    const [markdown, setMarkdown] = useState('');
+    useEffect(() => {
+        fetch('/help.md')
+            .then((response) => response.text())
+            .then((text) => setMarkdown(text));
+    }, []);
     return (
         <>
             <Head>
