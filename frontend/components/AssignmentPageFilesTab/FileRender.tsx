d43fdc8432a0a9d598e1b621ea54ac03b3b0eb62
Ido <benhaim.ido@gmail.com>
2024-03-02 18:58:41 -0500

    File render improvements

--- frontend/components/AssignmentPageFilesTab/FileRender.tsx
+++ frontend/components/AssignmentPageFilesTab/FileRender.tsx
@@ -5,11 +5,12 @@ import {
     MessageBarBody,
     Link,
 } from '@fluentui/react-components';
-import {extname} from 'path';
-import {Monaco} from '@/components';
+import { extname } from 'path';
+import { Monaco } from '@/components';
 import styles from './FileRender.module.css';
-import {useEffect, useState} from 'react';
-import filetype from 'magic-bytes.js'
+import { useEffect, useState } from 'react';
+import filetype from 'magic-bytes.js';
+import { GuessedFile } from 'magic-bytes.js/dist/model/tree';
 
 const languages = {
     js: 'javascript',
@@ -50,74 +51,92 @@ const videoExt = ['mp4', 'webm', 'ogg', 'avi', 'mov', 'wmv', 'flv', 'mkv', '3gp'
 const audioExt = ['mp3', 'wav', 'ogg', 'flac', 'aac', 'wma', 'm4a'];
 
 const ProcessedFileRender = ({
-                                 path,
-                                 content,
-                             }: {
+    path,
+    content,
+}: {
     path: string;
     content: ArrayBuffer;
 }) => {
     const pathExt = extname(path).slice(1);
     const fileTypes = filetype(new Uint8Array(content));
-
-    let ext: string | undefined =pathExt;
-    let fileType;
-    if(fileTypes.length > 0){
-        ext = fileTypes[0].extension;
+    let fileType: GuessedFile | undefined;
+    console.log(fileTypes);
+    let ext = pathExt;
+    if (fileTypes.length > 0) {
+        if (fileTypes[0].extension) {
+            ext = fileTypes[0].extension;
+        }
         fileType = fileTypes[0];
     }
     ext = ext?.toLowerCase();
 
     const [viewAnyway, setViewAnyway] = useState(false);
 
-
     useEffect(() => {
         setViewAnyway(false);
     }, [path, content]);
 
-    if(ext) {
-        if ((fileType && fileType.mime?.startsWith("image")) || imgExt.includes(ext)) {
+    if (ext) {
+        if ((fileType && fileType.mime?.startsWith('image')) || imgExt.includes(ext)) {
             return (
-                <Image
+                <div className={styles.centerImage}>
+                    <Image
+                        draggable="false"
+                        src={URL.createObjectURL(
+                            new Blob([content], {
+                                type:
+                                    fileType?.mime ||
+                                    `image/${fileType?.extension ?? ext}`,
+                            })
+                        )}
+                        alt={path}
+                        className={styles.renderedHeight}
+                    />
+                </div>
+            );
+        }
+        if ((fileType && fileType.mime?.startsWith('video')) || videoExt.includes(ext)) {
+            return (
+                <div className={styles.centerImage}>
+                    <video
+                        className={styles.renderedHeight}
+                        src={URL.createObjectURL(
+                            new Blob([content], {
+                                type:
+                                    fileType?.mime ??
+                                    `video/${fileType?.extension ?? ext}`,
+                            })
+                        )}
+                        draggable="false"
+                        controls
+                    ></video>
+                </div>
+            );
+        }
+        if ((fileType && fileType.mime?.startsWith('audio')) || audioExt.includes(ext)) {
+            return (
+                <audio
                     draggable="false"
-                    src={URL.createObjectURL(new Blob([content], {type: fileType?.mime || `image/${fileType?.extension ?? ext}`}))}
-                    alt={path}
-                    width="100%"
-                />
+                    controls
+                    src={URL.createObjectURL(
+                        new Blob([content], {
+                            type: fileType?.mime ?? `audio/${fileType?.extension ?? ext}`,
+                        })
+                    )}
+                ></audio>
             );
         }
-        if ((fileType && fileType.mime?.startsWith("video")) || videoExt.includes(ext)) {
-            return (
-                <video draggable="false"  controls width="100%" height="50vh">
-                    <source
-                        src={URL.createObjectURL(
-                            new Blob([content], {type: `video/${fileType?.extension ?? ext}`})
-                        )}
-                    />
-                </video>
-            );
-        }
-        if ((fileType && fileType.mime?.startsWith("audio")) || audioExt.includes(ext)) {
-            return (
-                <audio draggable="false" controls>
-                    <source
-                        src={URL.createObjectURL(
-                            new Blob([content], {type: `audio/${fileType?.extension ?? ext}`})
-                        )}
-                    />
-                </audio>
-            );
-        }
-        if (fileType?.mime === "application/pdf") {
+        if (fileType?.mime === 'application/pdf') {
             return (
                 <iframe
                     draggable="false"
                     src={URL.createObjectURL(
-                        new Blob([content], {type: `application/pdf`})
+                        new Blob([content], { type: `application/pdf` })
                     )}
                     title={path}
                     width="100%"
                     height="50vh"
-                    style={{height: '50vh'}}
+                    style={{ height: '50vh' }}
                 />
             );
         }
@@ -127,7 +146,7 @@ const ProcessedFileRender = ({
     const decoded = decoder.decode(content);
     if (!viewAnyway && /\ufffd/.test(decoded)) {
         return (
-            <MessageBar intent={'error'}>
+            <MessageBar className={styles.messagebar} intent={'error'}>
                 <MessageBarBody>
                     <MessageBarTitle>Binary Data</MessageBarTitle>
                     This file contains binary data that cannot be rendered.{' '}
@@ -140,7 +159,7 @@ const ProcessedFileRender = ({
         return (
             <Image
                 draggable="false"
-                src={URL.createObjectURL(new Blob([content], {type: `image/svg+xml`}))}
+                src={URL.createObjectURL(new Blob([content], { type: `image/svg+xml` }))}
                 alt={path}
             />
         );
@@ -155,14 +174,10 @@ const ProcessedFileRender = ({
     );
 };
 
-export const FileRender = ({path, content}: {
-    path: string;
-    content: ArrayBuffer
-}) => {
+export const FileRender = ({ path, content }: { path: string; content: ArrayBuffer }) => {
     return (
-        <div className={styles.filerenderer}
-             onClick={(event) => event.stopPropagation()}>
-            <ProcessedFileRender path={path} content={content}/>
+        <div className={styles.filerenderer} onClick={(event) => event.stopPropagation()}>
+            <ProcessedFileRender path={path} content={content} />
         </div>
     );
 };
